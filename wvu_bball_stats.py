34db3d9293b166bd23b9adcd6c5d0662f4259df9
Matt Swales <swales@gmail.com>
2018-03-31 21:52:45 -0400

    scrape
    scrape

--- wvu_bball_stats.py
+++ wvu_bball_stats.py
@@ -9,5 +9,13 @@ HEADERS = {'User-Agent': 'Mozilla/5.0'}
 def get_bball_stats_main():
     """Return parsed main men's bball stats"""
     response = requests.get(
+        BASE_URL + '/sport/mbasketball',
+        headers=HEADERS)
+    return BeautifulSoup(response.content, "lxml")
 
-    )
+soup = get_bball_stats_main()
+
+roster = soup.select_one(
+            'table.items.table.table-striped.table-bordered.table-condensed')
+
+roster.select('th')
